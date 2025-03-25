e0aa29677a84cfcdaccf640da0ef7703c8f6fe7c
yashpote21 <yashpote21@gmail.com>
2025-03-25 19:19:47 +0530

    All browser bat file updated ie. alluure report cmd removes

--- Bankapp_AllBrowsers_Run.bat
+++ Bankapp_AllBrowsers_Run.bat
@@ -1,3 +1,3 @@
-pytest -v -s --browser chrome --html="HTMLReports\Bankapp_Chrome.html" --alluredir="AllureReports" -p no:warnings
-pytest -v -s --browser edge --html="HTMLReports\Bankapp_Edge.html" --alluredir="AllureReports" -p no:warnings
-pytest -v -s --browser firefox --html="HTMLReports\Bankapp_Firefox.html" --alluredir="AllureReports" -p no:warnings
+pytest -v -s --browser chrome --html="HTMLReports\Bankapp_Chrome.html" -p no:warnings
+pytest -v -s --browser edge --html="HTMLReports\Bankapp_Edge.html" -p no:warnings
+pytest -v -s --browser firefox --html="HTMLReports\Bankapp_Firefox.html" -p no:warnings
